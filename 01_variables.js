7184e19d54a11d13a920f6be9f4b2387adf24dcf
Suryansh khare <kharesuryanshkhare@gmail.com>
2026-06-10 18:44:11 +0530

    this is edit1

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail= "kharesuryanshkhare@gmail.com"
+var accountPass ="12345"
+
+accountcity ="Jaipur"
+// accountId =34
+accountEmail="holahola@gmail.com"
+accountPass="hello"
+accountcity="varansi"
+
+let accstate;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountcity,accstate])
+
+
