85ca02ba8ba9de3a77e82452420eab32eca4e726
Suryansh khare <kharesuryanshkhare@gmail.com>
2026-06-10 21:33:48 +0530

    upate is coming

---
+++ 06_numsandmath.js
@@ -0,0 +1,13 @@
+const score =400
+console.log(score)
+
+const balance=new Number(300);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(4));
+
+const othernumber=123.8966
+console.log(othernumber.toPrecision(2))
+// use this with caution it prioritises the value before decimal
+
---
+++ 07_dates.js
@@ -0,0 +1,20 @@
+let mydate=new Date()
+console.log(mydate)
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toTimeString())
+
+console.log(typeof mydate)
+
+let newdate=new Date(2026,0,8);
+console.log(newdate.toDateString())
+let newdate2=new Date(2026-01-08);
+console.log(newdate2.toDateString())
+
+let mytimestamp = Date.now();
+console.log(mytimestamp);
+console.log(Math.floor(Date.now()/1000));
+
+newdate.toLocaleString('default', {
+    weekday: "long"
+})
