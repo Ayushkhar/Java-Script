7ab73e6424248b1574b6dece08135964a4ec0d2e
Suryansh khare <kharesuryanshkhare@gmail.com>
2026-06-14 16:53:43 +0530

    Projects

---
+++ Projects/2-BMICalculator/bmi.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    const height = parseInt(document.getElementById('height').value);
+    const weight =parseInt(document.getElementById('weight').value);
+    const result = document.getElementById('results');
+
+    if(height<0 || isNaN(height) || height==='')
+    {
+        result.innerHTML='please enter a valid height';
+    }
+    else if(weight<0 || isNaN(weight) || weight==='')
+    {
+        result.innerHTML='please enter a valid weight';
+
+    }else{
+        const bmi =(weight / ((height * height) / 10000)).toFixed(2);
+        result.innerHTML=`<span>${bmi}</span>`
+    }
+})
